6a5c5d095ddd72eb489c280a7fd1a4ba14311748
himanshmunjal <munjalhimansh2211@gmail.com>
2026-03-18 17:51:45 +0530

    Initial Commit

---
+++ Travelitinerary/TravelitineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelitineraryApp.swift
+//  Travelitinerary
+//
+//  Created by Chandan Munjal on 18/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelitineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelitineraryTests/TravelitineraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelitineraryTests.swift
+//  TravelitineraryTests
+//
+//  Created by Chandan Munjal on 18/03/26.
+//
+
+import Testing
+@testable import Travelitinerary
+
+struct TravelitineraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
